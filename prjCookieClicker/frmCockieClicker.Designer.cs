690611830baa4c5b256b93e0f2dbee67a8203efb
Luka Verbrugghe <lotrdetailsys@gmail.com>
2024-03-12 14:50:43 +0100

    Daan

--- prjCookieClicker/frmCockieClicker.Designer.cs
+++ prjCookieClicker/frmCockieClicker.Designer.cs
@@ -28,15 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.lblAantalKinderen = new System.Windows.Forms.Label();
             this.grbUpgrades = new System.Windows.Forms.GroupBox();
+            this.lblUpgrade1VerdundKippenvoerPrijs = new System.Windows.Forms.Label();
+            this.picUpgrade1VerdundKippenvoer = new System.Windows.Forms.PictureBox();
             this.lblUpgrade1VerdundKippenvoer = new System.Windows.Forms.Label();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.picCock = new System.Windows.Forms.PictureBox();
-            this.lblUpgrade1VerdundKippenvoerPrijs = new System.Windows.Forms.Label();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.lblKinderenPerSeconde = new System.Windows.Forms.Label();
+            this.tmrExtraKinderen = new System.Windows.Forms.Timer(this.components);
             this.grbUpgrades.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUpgrade1VerdundKippenvoer)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picCock)).BeginInit();
             this.SuspendLayout();
@@ -54,7 +58,7 @@
             // grbUpgrades
             // 
             this.grbUpgrades.Controls.Add(this.lblUpgrade1VerdundKippenvoerPrijs);
-            this.grbUpgrades.Controls.Add(this.pictureBox2);
+            this.grbUpgrades.Controls.Add(this.picUpgrade1VerdundKippenvoer);
             this.grbUpgrades.Controls.Add(this.lblUpgrade1VerdundKippenvoer);
             this.grbUpgrades.Location = new System.Drawing.Point(489, 19);
             this.grbUpgrades.Name = "grbUpgrades";
@@ -62,6 +66,26 @@
             this.grbUpgrades.TabIndex = 3;
             this.grbUpgrades.TabStop = false;
             this.grbUpgrades.Text = "Upgrades";
+            // 
+            // lblUpgrade1VerdundKippenvoerPrijs
+            // 
+            this.lblUpgrade1VerdundKippenvoerPrijs.AutoSize = true;
+            this.lblUpgrade1VerdundKippenvoerPrijs.Location = new System.Drawing.Point(59, 37);
+            this.lblUpgrade1VerdundKippenvoerPrijs.Name = "lblUpgrade1VerdundKippenvoerPrijs";
+            this.lblUpgrade1VerdundKippenvoerPrijs.Size = new System.Drawing.Size(88, 13);
+            this.lblUpgrade1VerdundKippenvoerPrijs.TabIndex = 2;
+            this.lblUpgrade1VerdundKippenvoerPrijs.Text = "Prijs: 15 kinderen";
+            // 
+            // picUpgrade1VerdundKippenvoer
+            // 
+            this.picUpgrade1VerdundKippenvoer.Image = global::prjCookieClicker.Properties.Resources.Kippenvoer;
+            this.picUpgrade1VerdundKippenvoer.Location = new System.Drawing.Point(12, 17);
+            this.picUpgrade1VerdundKippenvoer.Name = "picUpgrade1VerdundKippenvoer";
+            this.picUpgrade1VerdundKippenvoer.Size = new System.Drawing.Size(38, 37);
+            this.picUpgrade1VerdundKippenvoer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picUpgrade1VerdundKippenvoer.TabIndex = 1;
+            this.picUpgrade1VerdundKippenvoer.TabStop = false;
+            this.picUpgrade1VerdundKippenvoer.Click += new System.EventHandler(this.picUpgrade1VerdundKippenvoer_Click);
             // 
             // lblUpgrade1VerdundKippenvoer
             // 
@@ -71,16 +95,6 @@
             this.lblUpgrade1VerdundKippenvoer.Size = new System.Drawing.Size(103, 13);
             this.lblUpgrade1VerdundKippenvoer.TabIndex = 0;
             this.lblUpgrade1VerdundKippenvoer.Text = "Verdund kippenvoer";
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = global::prjCookieClicker.Properties.Resources.Kippenvoer;
-            this.pictureBox2.Location = new System.Drawing.Point(12, 17);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(38, 37);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
             // 
             // pictureBox1
             // 
@@ -103,20 +117,37 @@
             this.picCock.TabStop = false;
             this.picCock.Click += new System.EventHandler(this.picCock_Click);
             // 
-            // lblUpgrade1VerdundKippenvoerPrijs
+            // btnHelp
             // 
-            this.lblUpgrade1VerdundKippenvoerPrijs.AutoSize = true;
-            this.lblUpgrade1VerdundKippenvoerPrijs.Location = new System.Drawing.Point(59, 37);
-            this.lblUpgrade1VerdundKippenvoerPrijs.Name = "lblUpgrade1VerdundKippenvoerPrijs";
-            this.lblUpgrade1VerdundKippenvoerPrijs.Size = new System.Drawing.Size(88, 13);
-            this.lblUpgrade1VerdundKippenvoerPrijs.TabIndex = 2;
-            this.lblUpgrade1VerdundKippenvoerPrijs.Text = "Prijs: 15 kinderen";
+            this.btnHelp.Location = new System.Drawing.Point(13, 419);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(20, 19);
+            this.btnHelp.TabIndex = 4;
+            this.btnHelp.Text = "?";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // lblKinderenPerSeconde
+            // 
+            this.lblKinderenPerSeconde.AutoSize = true;
+            this.lblKinderenPerSeconde.Location = new System.Drawing.Point(58, 6);
+            this.lblKinderenPerSeconde.Name = "lblKinderenPerSeconde";
+            this.lblKinderenPerSeconde.Size = new System.Drawing.Size(114, 13);
+            this.lblKinderenPerSeconde.TabIndex = 5;
+            this.lblKinderenPerSeconde.Text = "Kinderen per seconde:";
+            // 
+            // tmrExtraKinderen
+            // 
+            this.tmrExtraKinderen.Tick += new System.EventHandler(this.tmrExtraKinderen_Tick);
             // 
             // frmCockieClicker
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblKinderenPerSeconde);
+            this.Controls.Add(this.btnHelp);
             this.Controls.Add(this.grbUpgrades);
             this.Controls.Add(this.lblAantalKinderen);
             this.Controls.Add(this.pictureBox1);
@@ -125,7 +156,7 @@
             this.Text = "Cock Clicker";
             this.grbUpgrades.ResumeLayout(false);
             this.grbUpgrades.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUpgrade1VerdundKippenvoer)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picCock)).EndInit();
             this.ResumeLayout(false);
@@ -139,9 +170,12 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label lblAantalKinderen;
         private System.Windows.Forms.GroupBox grbUpgrades;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox picUpgrade1VerdundKippenvoer;
         private System.Windows.Forms.Label lblUpgrade1VerdundKippenvoer;
         private System.Windows.Forms.Label lblUpgrade1VerdundKippenvoerPrijs;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.Label lblKinderenPerSeconde;
+        private System.Windows.Forms.Timer tmrExtraKinderen;
     }
 }
 
--- prjCookieClicker/frmCockieClicker.cs
+++ prjCookieClicker/frmCockieClicker.cs
@@ -13,17 +13,79 @@ namespace prjCookieClicker
     public partial class frmCockieClicker : Form
     {
         int intAantalKinderen = 0;
+        int intAantalKinderenPerSeconde = 0;
+
+        int intVereisteKinderenUpgrade1VerdundKippenvoerPrijs = 15;
+        int intVereisteKinderenUpgrade1VerdundKippenvoerExtraKinderenPerSeconde = 1;
 
         public frmCockieClicker()
         {
             InitializeComponent();
+            UpdateScherm();
         }
 
         private void picCock_Click(object sender, EventArgs e)
         {
             //er is op de cock geklikt
             intAantalKinderen++;
+            UpdateScherm();
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            //messegebox met uitleg weergeven
+            MessageBox.Show("Klik op de cock om kinderen te produceren.\nKoop met kinderen upgrades om de kinderupgrade te automatiseren.", "Help");
+        }
+
+        private void picUpgrade1VerdundKippenvoer_Click(object sender, EventArgs e)
+        {
+            if (IsUpgradeBeschikbaar(intAantalKinderen, intVereisteKinderenUpgrade1VerdundKippenvoerPrijs))
+            {
+                //verdund kippenvoer gekocht
+                intAantalKinderen = intAantalKinderen - intVereisteKinderenUpgrade1VerdundKippenvoerPrijs;
+                intAantalKinderenPerSeconde = intAantalKinderenPerSeconde + intVereisteKinderenUpgrade1VerdundKippenvoerExtraKinderenPerSeconde;
+                UpdateScherm();
+                BerekenIntervalTimerExtraKinderen();
+                tmrExtraKinderen.Start();
+            }
+            else
+            {
+                //de gebruiker is te skeer om dit te kunnen betalen
+                MessageBox.Show("U bent te skeer om dit te kunnen kopen.", "U bent skeer!");
+            }
+        }
+
+
+        //functie om te checken of de upgrade nog beschikbaar is
+        private bool IsUpgradeBeschikbaar(int intAanatalKinderen, int intVereisteKinderen)
+        {
+            if (intAanatalKinderen >= intVereisteKinderen)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //functie om labels te updaten
+        private void UpdateScherm()
+        {
             lblAantalKinderen.Text = "Aantal kinderen: " + intAantalKinderen.ToString();
+            lblKinderenPerSeconde.Text = "Kinderen per seconde: " + intAantalKinderenPerSeconde.ToString();
+        }
+
+        private void tmrExtraKinderen_Tick(object sender, EventArgs e)
+        {
+            //plus 1 kind
+            intAantalKinderen++;
+            UpdateScherm();
+        }
+
+        private void BerekenIntervalTimerExtraKinderen()
+        {
+            tmrExtraKinderen.Interval = 1000 / intAantalKinderenPerSeconde;
         }
     }
 }
